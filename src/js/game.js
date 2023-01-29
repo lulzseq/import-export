8027a228469241d5716e7f77d462a01f1d71d10b
lulzseq <teoahoo@gmail.com>
2023-01-29 13:33:40 +0800

    Add files

--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,11 @@
 import Character from './domain.js';
 
 class Game {
-    start() {
-      console.log('game started');
-    }
+  start() {
+    console.log('game started');
   }
-  
+}
+
 class GameSavingData {
 }
 
@@ -15,4 +15,6 @@ function readGameSaving() {
 function writeGameSaving() {
 }
 
-export { Game as default, GameSavingData, readGameSaving, writeGameSaving }
+export {
+  Game as default, GameSavingData, readGameSaving, writeGameSaving,
+};
